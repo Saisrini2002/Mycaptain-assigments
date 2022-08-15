3876826be6ebf6ee90c7598006d79907db0d03a1
Saisrini2002 <110685343+Saisrini2002@users.noreply.github.com>
2022-08-15 21:04:52 +0530

    Add files via upload

---
+++ Sai_Area.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 22:11:27 2022
+
+@author: Sai 
+"""
+
+print("Area Of The Circle")
+r = float(input("Radius : "))
+a = 3.14159 * r * r
+print("Area : ", a)
+
+ i=1
+     while 
+         True: if i%0O7 == 0: 
+             break print(i) i += 1
