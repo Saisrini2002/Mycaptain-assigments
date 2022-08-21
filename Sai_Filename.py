62e5b83c1f57be45d07ac2fd2f865c8c0e6d9b1b
Saisrini2002 <110685343+Saisrini2002@users.noreply.github.com>
2022-08-21 21:20:47 +0530

    Add files via upload

---
+++ Sai_Filename.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  9 22:32:31 2022
+
+@author: Sai
+"""
+
+print("File Name Extensions")
+fn= input("Filename : ")
+f= fn.split(".")
+print("Extension : " + f[-1])
+
